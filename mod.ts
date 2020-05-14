fbd2cd0a09bb6d168c32868eaac464f97d260831
Helder Santana <git@helder.tech>
2020-05-14 04:20:24 -0300

    first commit

---
+++ mod.ts
@@ -0,0 +1,8 @@
+import { fromUint8Array, lookup } from "./deps.ts";
+
+/** Returns `Hello World` in bold */
+export async function datauri(filePath: string): Promise<string> {
+  const content = await Deno.readFile(filePath);
+
+  return `data:${lookup(filePath)};base64,${fromUint8Array(content)}`;
+}
